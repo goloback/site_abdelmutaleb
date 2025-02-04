efbd85ae6b5532d4cb3a633a2b069a019727fe84
goloback <adduma2008@gmail.com>
2025-02-04 18:16:52 +0200

    add translator

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ var engineer_price = '15$/1h';
 var pass_rus_exams = 'student'
 var pass_rus_exams_img = 'files/hat.png';
 var pass_rus_exams_price = '10$/exam';
-
+var translator = 'translator';
+var translator_img = 'files/translate.png';
+var translator_price = '5$/1list';
+var translator_price2 = '15$/1h';
 var name_page = 'products';
 
-var job = [engineer, pass_rus_exams];
+var job = [engineer, pass_rus_exams, translator];
 
-var job_img = [engineer_img, pass_rus_exams_img];
+var job_img = [engineer_img, pass_rus_exams_img, translator_img];
 
-var job_price = [engineer_price, pass_rus_exams_price];
+var job_price = [engineer_price, pass_rus_exams_price, [translator_price, translator_price2]];
 
 var current_job = 0;
 
@@ -29,15 +32,61 @@ function next_page(){
         else{
             current_job++;
         }
-        var name_product = document.getElementsByClassName("name_product")[0];
-        name_product.textContent = job[current_job];
-        var img_product = document.getElementsByClassName("img_product")[0];
-        img_product.src = job_img[current_job];
-        var price = document.getElementsByClassName("price")[0];
-        price.textContent = job_price[current_job];
+        show_current_job();
     }
 
 }
+
+function prev_page(){
+    if (name_page == 'products'){
+        if(current_job == 0){
+            current_job = job.length-1;
+        }
+        else{
+            current_job--;
+        }
+        show_current_job();
+    }
+
+}
+function show_current_job(){
+    var mainpart = document.getElementsByClassName('main-part')[0];
+    var luanguage = document.getElementsByClassName('luanguage');
+    if (luanguage.length == 1){
+        mainpart.removeChild(luanguage[0]);
+    }
+    var name_product = document.getElementsByClassName("name_product")[0];
+    name_product.textContent = job[current_job];
+    var img_product = document.getElementsByClassName("img_product")[0];
+    img_product.src = job_img[current_job];
+    var price = document.getElementsByClassName("price")[0];
+
+    if(job[current_job] == translator){
+        var price1 = document.createElement('div');       
+        price.textContent = '';
+        price.appendChild(price1);
+        price1.textContent = job_price[current_job][0];
+        price1.style.position = 'absolute';
+        price1.style.left = '15vw';
+        price1.style.bottom = '-5vh';
+        var price2 = document.createElement('div');
+        price.appendChild(price2);
+        price2.textContent = job_price[current_job][1];
+        price2.style.position = 'absolute';
+        price2.style.right = '15vw';
+        price2.style.bottom = '-5vh';
+        var discription = document.createElement('div');
+        discription.textContent = 'Arabic-English, Arabic-Russian, English-Russian';
+        discription.classList.add('luanguage');
+        mainpart.appendChild(discription);
+ 
+    }
+    else{
+        price.textContent = job_price[current_job];
+    }
+}
+        
+
 
 function create_content_products(){
     var name_product = document.getElementsByClassName("name_product")[0];
